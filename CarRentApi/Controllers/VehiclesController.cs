d74c961b67ff8c796c62a7b1e53b8b04c8449121
Andrei Ardel <ardel_andrei@yahoo.com>
2020-10-15 15:06:43 +0300

    Remove unused classes and create dummy controller.

---
+++ CarRentApi/Controllers/VehiclesController.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class VehiclesController : ControllerBase
+    {
+
+        public IActionResult Index()
+        {
+            var message = new { Message = "Welcome to rent a car!" };
+
+            return Ok(JsonSerializer.Serialize(message));
+        }
+    }
+}
