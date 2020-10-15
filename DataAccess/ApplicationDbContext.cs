f8206727836bae368a148f9c3a4dce33a0dce672
Andrei Ardel <ardel_andrei@yahoo.com>
2020-10-15 15:08:34 +0300

    Remove unused namespaces.

--- DataAccess/ApplicationDbContext.cs
+++ DataAccess/ApplicationDbContext.cs
@@ -1,9 +1,6 @@
 ﻿using DataAccess.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Text;
 
 namespace DataAccess
 {
